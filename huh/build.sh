c3f92a4b39327e8a87080c8efa34b6783b5e0178
Josh Long <josh@joshlong.com>
2020-04-15 18:07:07 -0700

    reactive

--- huh/build.sh
+++ huh/build.sh
@@ -1,3 +1,11 @@
-#mvn clean package
+mvn -DskipTests=true clean package
+export MI=src/main/resources/META-INF
+mkdir -p $MI 
+
 java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar target/traditional.jar
+## curl localhost:8080/reservations  !! 
+tree $MI
 mvn -Pgraal clean package
+
+
+# mvn -Pgraal clean package
